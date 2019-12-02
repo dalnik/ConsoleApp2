e1b7ee13a2bee739edb7863dc301c63ebd5b970e
dalnik61327@gmail.com <dalnik61327@gmail.com>
2019-12-02 19:30:22 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //запросить имя пользователя
+            Console.Write("Введите имя пользователя и нажмите Enter:");
+
+
+            //прочесть данные и присвоитьи мя переменной \"name\"
+            string name = Console.ReadLine();
+
+            // \"name\" можно использовать как обычную переменную
+            Console.WriteLine("Hello, " + name + "!");
+
+            // Используем ReadLine() с численными типами
+             Console.Write("Введите свой возраст и нажмите Enter: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите свой рост и нажмите Enter: ");
+
+
+            double height = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Вам {0} лет, а рост {1}.", age, height);
+
+                // А теперь попросим пользователя нажать \"Enter\" для выхода из программы
+              Console.Write("Программа завершена. Нажмите Enter для выхода...");
+              Console.ReadLine();
+        }
+
+        }
+    }
+
